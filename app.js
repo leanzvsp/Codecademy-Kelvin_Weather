dc21499c991330ad953a96548efc4b5c9d577282
Leandro Marin <marinlopezleandro@gmail.com>
2022-08-25 18:45:30 -0300

    Adding convert to Newton degrees

--- app.js
+++ app.js
@@ -4,5 +4,17 @@ const kelvin = 293;
   //Convert Kelvin in Celsius
 let celsius = kelvin - 273;
 
+//Celsius to Fahrenheit
 let fahrenheit = celsius * (9/5) + 32;
 
+//Round down Fahrenheit temperature
+fahrenheit = Math.floor(fahrenheit);
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
+
+//Convert Celsius to Newton
+let newton = celsius * (33/100);
+
+newton = Math.floor(newton);
+
+console.log(`The temperature is ${newton} degrees Newton`);
